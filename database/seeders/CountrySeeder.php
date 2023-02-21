a23bc3ccb3c8f3ad7e555dfe2462cf4620f80c91
AyumuOgasawara <ogasaayumu0114@gmail.com>
2023-02-21 20:16:57 +0900

    Laravelの環境構築&MigrationによるPost,Category,Country Table, Modelの作成、リレーションの実装&seedingによるレコードの追加&indexファイルに国名を表示

---
+++ database/seeders/CountrySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('countries')->insert([
+                'country_name' => '日本',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ]);
+        DB::table('countries')->insert([
+                'country_name' => '英国',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ]);
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Traveler</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Choose Country</h1>
+        <div class='countries'>
+            @foreach ($posts as $post)
+                <div class='country'>
+                    <h2 class='country_name'>
+                        <a href="">{{ $post->country->country_name }}</a>
+                    </h2>
+                </div>
+            @endforeach
+        </div>
+    </body>
+</html>
