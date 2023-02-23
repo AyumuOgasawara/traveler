2ac69e3da22e53865c0530c96682a1309840f779
AyumuOgasawara <ogasaayumu0114@gmail.com>
2023-02-23 18:58:27 +0900

    新規投稿のバリデーション(PostRequest使用)

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:255',
+            'body' => 'required|max:4000',
+            'category_id' => 'required',
+        ];
+    }
+}
