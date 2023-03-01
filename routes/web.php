642d46aa6741b009cf2e005182503867f628c401
AyumuOgasawara <ogasaayumu0114@gmail.com>
2023-03-01 15:26:38 +0900

    認証機能の追加

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,56 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CountryController;
 use App\Http\Controllers\CategoryController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::controller(CountryController::class)->middleware(['auth'])->group(function(){
+   Route::get('/', 'index')->name('index');
+   Route::get('/countries/{country}', 'show')->name('show');
+});
+
+Route::controller(PostController::class)->middleware(['auth'])->group(function(){
+    Route::get('/countries/{country}/posts/create', 'create')->name('create');
+    Route::post('/countries/{country}/posts', 'store')->name('store');
+    Route::get('/posts/{post}', 'detail')->name('detail');
+    Route::get('/posts/{post}/edit', 'edit')->name('edit');
+    Route::put('/posts/{post}', 'update')->name('update');
+    Route::delete('/posts/{post}', 'delete')->name('delete');
+});
+
+Route::controller(CategoryController::class)->middleware(['auth'])->group(function(){
+    Route::get('/countries/{country}/categories/{category}', 'index')->name('category_index');
+});
 
 
-Route::get('/', [CountryController::class, 'index']);
-Route::get('/countries/{country}', [CountryController::class,'show'])->name('countries.show');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/countries/{country}/posts/create', [PostController::class, 'create']);
-Route::post('/countries/{country}/posts', [PostController::class, 'store']);
-Route::get('/posts/{post}', [PostController::class, 'detail']);
-Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
-Route::put('/posts/{post}', [PostController::class, 'update']);
-Route::delete('/posts/{post}', [PostController::class, 'delete']);
 
-Route::get('countries/{country}/categories/{category}', [CategoryController::class, 'index']);
+
+
+require __DIR__.'/auth.php';
