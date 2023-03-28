347be5b8280e71c5487791c6d40c223710adc508
AyumuOgasawara <ogasaayumu0114@gmail.com>
2023-03-28 22:04:48 +0900

    投稿画面の修正&アカウント削除の追加

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -16,32 +16,5 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('posts')->insert([
-                'title' => '日本の食事',
-                'body' => '日本の料理「和食」はすごく美味しいです。',
-                'user_id' => 1,
-                'category_id' => 1,
-                'country_id' => 1,
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime(),
-            ]);
-        DB::table('posts')->insert([
-                'title' => '東京タワー',
-                'body' => '東京タワーは夜になるとすごき綺麗です。',
-                'user_id' => 2,
-                'category_id' => 2,
-                'country_id' => 1,
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime(),
-            ]);
-        DB::table('posts')->insert([
-                'title' => 'Big Ben',
-                'body' => 'イギリスの象徴であるBig Benはテムズ川の横にあります。',
-                'user_id' => 1,
-                'category_id' => 2,
-                'country_id' => 2,
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime(),
-            ]);
     }
 }
