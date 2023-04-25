6a237413399e98178e6d73ad7ab4cb686fb33c00
AyumuOgasawara <ogasaayumu0114@gmail.com>
2023-04-26 01:20:19 +0900

    editファイル,createファイルのcssの改良&php.iniのuploadableサイズを大きくした

--- resources/views/home/edit.blade.php
+++ resources/views/home/edit.blade.php
@@ -5,47 +5,60 @@
             <meta charset="utf-8">
             <title>Traveler</title>
             <!-- Fonts -->
+            <link href="{{ secure_asset('/css/create_edit.css') }}" rel="stylesheet">
             <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         </head>
         <body>
-            <h1>編集画面</h1>
-            <form action="/home/{{$post->id}}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
-                <div class="content_title">
-                    <h2>Title</h2>
-                    <input type="text" id="title" name="title" value="{{ $post->title }}"/>
-                    <p class="title__error" style="color:red">{{ $errors->first('title') }}</p>
-                </div>
-                <div class='content_body'>
-                    <h2>Body</h2>
-                    <textarea id="body" name="body" >{{ $post->body }}</textarea>
-                    <p class="body__error" style="color:red">{{ $errors->first('body') }}</p>
-                </div>
-                <div class='image'>
-                    <h2>画像をアップロード</h2>
-                    
-                    <div class="image">
-                        @if ($post->image)
-                            <image id='old_img' src={{ $post->image }} width="50%" alt="画像が読み取れません"/>
-                        @endif    
+            <div class="big_title_container">
+                <h1 class="big_title">編集画面</h1>
+            </div>
+            <div class="post_container">
+                <form class="post" action="/home/{{$post->id}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="title_container">
+                        <h2 class="title left_container">タイトル</h2>
+                        <div class="right_container">
+                            <input class="input" type="text" id="title" name="title" value="{{ $post->title }}"/>
+                            <p class="title__error" style="color:red">{{ $errors->first('title') }}</p>
+                        </div>
                     </div>
-                    <input type="file" id= "image" name="image" onchange="ChangeHidden()" value="{{ old('image') }}"/>
-                    <p class="image__error" style="color:red">{{ $errors->first('image') }}</p>
-                </div>
-                <div>
-                    <h2>Category</h2>
-                    <select name="category_id">
-                          @foreach($categories as $category)
-                            <option value="{{ $category->id }}">{{ $category->category_name }}</option>
-                          @endforeach
-                    </select>
-                </div>
-                <button type="submit">更新</button>
-                <div class='footer'>
-                    <a href="/home/{{$post->id}}">戻る</a>
-                </div>
-            </form>
+                    <div class='body_container'>
+                        <h2 class="body left_container">内容</h2>
+                        <div class="right_container">
+                            <textarea class="input" id="body" name="body" >{{ $post->body }}</textarea>
+                            <p class="body__error" style="color:red">{{ $errors->first('body') }}</p>
+                        </div>
+                    </div>
+                        <div class="image_container">
+                            <h2 class="image left_container">
+                                画像を
+                                <p>アップロード</p>
+                            </h2>
+                            <div class="right_container post_images">
+                            @if ($post->image)
+                                <image class="edit_old_img" id='old_img' src={{ $post->image }} alt="画像が読み取れません"/>
+                            @endif
+                            <input type="file" class="edit_image" id= "image" name="image" onchange="ChangeHidden()" value="{{ old('image') }}"/>
+                            <p class="image__error" style="color:red">{{ $errors->first('image') }}</p>
+                            </div>
+                        </div>
+                    <div class="category_container">
+                        <h2 class="category left_container">カテゴリー</h2>
+                        <div class="right_container"> 
+                            <select name="category_id">
+                                  @foreach($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                  @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <button type="submit" class="update_share_btn">更新</button>
+                    <div class='footer'>
+                        <a href="/home/{{$post->id}}" class="back_btn">戻る</a>
+                    </div>
+                </form>
+            </div>
             <script type="text/javascript">
                 function ChangeHidden() {
                   const elem = document.getElementById('old_img');
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,40 +5,58 @@
             <meta charset="utf-8">
             <title>Traveler</title>
             <!-- Fonts -->
+            <link href="{{ secure_asset('/css/create_edit.css') }}" rel="stylesheet">
             <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         </head>
         <body>
-            <h1>お気に入りをシェアしよう</h1>
-            <form action="/countries/{{$country->id}}/posts" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="title">
-                    <h2>Title</h2>
-                    <input type="text" id="title" name="title" placeholder="タイトル" value="{{ old('title')}}"/>
-                    <p class="title__error" style="color:red">{{ $errors->first('title') }}</p>
-                </div>
-                <div class='body'>
-                    <h2>Body</h2>
-                    <textarea id="body" name="body" placeholder="おすすめについて詳しく教えて">{{ old('body')}}</textarea>
-                    <p class="body__error" style="color:red">{{ $errors->first('body') }}</p>
-                </div>
-                <div class='image'>
-                    <h2>画像をアップロード</h2>
-                    <input type="file" id= "image" name="image" value="{{ old('image') }}"/>
-                    <p class="image__error" style="color:red">{{ $errors->first('image') }}</p>
-                </div>
-                <div>
-                    <h2>Category</h2>
-                    <select name="category_id">
-                          @foreach($categories as $category)
-                            <option value="{{ $category->id }}">{{ $category->category_name }}</option>
-                          @endforeach
-                    </select>
-                </div>
-                <button type="submit">シェアする</button>
-                <div class='footer'>
-                    <a href="/countries/{{$country->code}}">戻る</a>
-                </div>
-            </form>
+            
+            <div class="big_title_container">
+                <h1 class="big_title">ブログ投稿</h1>
+            </div>
+            <div class="post_container">
+                <form class="post" action="/countries/{{$country->id}}/posts" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="title_container">
+                        <h2 class="title left_container">タイトル</h2>
+                        <div class="right_container">
+                            <input class="input" type="text" id="title" name="title" placeholder="タイトル" value="{{ old('title')}}"/>
+                            <p class="title__error" style="color:red">{{ $errors->first('title') }}</p>
+                        </div>
+                    </div>
+                    <div class='body_container'>
+                        <h2 class="body left_container">内容</h2>
+                        <div class="right_container">
+                            <textarea class="input" id="body" name="body" placeholder="おすすめについて詳しく教えて">{{ old('body')}}</textarea>
+                            <p class="body__error" style="color:red">{{ $errors->first('body') }}</p>
+                        </div>
+                    </div>
+                    <div class='image_container'>
+                        <h2 class="image left_container">
+                            画像を
+                            <p>アップロード</p>
+                        </h2>
+                        <div class="right_container">    
+                            <input type="file" id= "image" name="image" value="{{ old('image') }}"/>
+                            <p class="image__error" style="color:red">{{ $errors->first('image') }}</p>
+                        </div>
+                    </div>
+                    <div class="category_container">
+                        <h2 class="category left_container">カテゴリー</h2>
+                        <div class="right_container">    
+                            <select  name="category_id">
+                                  @foreach($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                  @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <button type="submit" class="update_share_btn">シェアする</button>
+                    <div class='footer'>
+                        <a href="/countries/{{$country->code}}" class="back_btn">戻る</a>
+                    </div>
+                </form>
+            </div>    
+            
         </body>
     </html>
 </x-app-layout>
